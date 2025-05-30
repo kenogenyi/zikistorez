26ea31aedef32e5bb7269880b527dc4d0027398c
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-05-31 00:19:42 +0100

    Update trpc.ts

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -3,12 +3,14 @@ import { ExpressContext } from '@/server'
 import { TRPCError, initTRPC } from '@trpc/server'
 import { PayloadRequest } from 'payload/types'
 
+// Initialize TRPC with Express context
 const t = initTRPC.context<ExpressContext>().create()
+
 const middleware = t.middleware
 
+// Middleware for authentication check
 const isAuth = middleware(async ({ ctx, next }) => {
   const req = ctx.req as PayloadRequest
-
   const { user } = req as { user: User | null }
 
   if (!user || !user.id) {
@@ -22,6 +24,8 @@ const isAuth = middleware(async ({ ctx, next }) => {
   })
 })
 
+// Export helpers
 export const router = t.router
 export const publicProcedure = t.procedure
 export const privateProcedure = t.procedure.use(isAuth)
+
