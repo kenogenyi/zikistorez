0bf5fadc387f5b64c777c6ccfb858eea1b1bed86
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-05-30 23:22:59 +0100

    Update Media.ts

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,39 +1,41 @@
-import { User } from '../payload-types'
-import { Access, CollectionConfig } from 'payload/types'
+import { User } from '../payload-types';
+import { Access, CollectionConfig } from 'payload/types';
 
-const isAdminOrHasAccessToImages =
-  (): Access =>
-  async ({ req }) => {
-    const user = req.user as User | undefined
+const isAdminOrHasAccessToImages = (): Access => async ({ req }) => {
+  const user = req.user as User | undefined;
 
-    if (!user) return false
-    if (user.role === 'admin') return true
+  if (!user) return false;
+  if (user.role === 'admin') return true;
 
-    return {
-      user: {
-        equals: req.user.id,
-      },
-    }
-  }
+  return {
+    user: {
+      equals: req.user.id,
+    },
+  };
+};
 
 export const Media: CollectionConfig = {
   slug: 'media',
   hooks: {
     beforeChange: [
       ({ req, data }) => {
-        return { ...data, user: req.user.id }
+        if (req.user) {
+          return { ...data, user: req.user.id };
+        }
+        return data;
       },
     ],
   },
   access: {
+    create: ({ req }) => !!req.user, // ✅ allow uploads for authenticated users
     read: async ({ req }) => {
-      const referer = req.headers.referer
+      const referer = req.headers.referer;
 
       if (!req.user || !referer?.includes('sell')) {
-        return true
+        return true;
       }
 
-      return await isAdminOrHasAccessToImages()({ req })
+      return await isAdminOrHasAccessToImages()({ req });
     },
     delete: isAdminOrHasAccessToImages(),
     update: isAdminOrHasAccessToImages(),
@@ -78,4 +80,4 @@ export const Media: CollectionConfig = {
       },
     },
   ],
-}
+};
