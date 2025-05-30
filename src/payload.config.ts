89d2689e43778e73e712dbfacb70f0aa3534cb2e
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-05-30 19:47:33 +0100

    fullback

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -72,14 +72,14 @@ export default buildConfig({
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
   email: {
-  fromName: process.env.EMAIL_FROM_NAME,
-  fromAddress: process.env.EMAIL_FROM_ADDRESS,
+  fromName: process.env.EMAIL_FROM_NAME || '',
+  fromAddress: process.env.EMAIL_FROM_ADDRESS || '',
   transportOptions: {
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
+    host: process.env.SMTP_HOST || '',
+    port: Number(process.env.SMTP_PORT || 587),
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.SMTP_USER || '',
+      pass: process.env.SMTP_PASS || '',
     },
   },
 },
