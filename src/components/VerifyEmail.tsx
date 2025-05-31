2f2dee1551f06f99914af65d276e9ee87750c959
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-05-31 01:02:02 +0100

    Update VerifyEmail.tsx

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -11,21 +11,17 @@ interface VerifyEmailProps {
 }
 
 const VerifyEmail = ({ token }: VerifyEmailProps) => {
-  const { data, isLoading, isError } =
-    trpc.auth.verifyEmail.useQuery({
-      token,
-    })
+  const { data, isLoading, isError } = trpc.auth.verifyEmail.useQuery({
+    token,
+  })
 
   if (isError) {
     return (
       <div className='flex flex-col items-center gap-2'>
         <XCircle className='h-8 w-8 text-red-600' />
-        <h3 className='font-semibold text-xl'>
-          There was a problem
-        </h3>
+        <h3 className='font-semibold text-xl'>There was a problem</h3>
         <p className='text-muted-foreground text-sm'>
-          This token is not valid or might be expired.
-          Please try again.
+          This token is not valid or might be expired. Please try again.
         </p>
       </div>
     )
@@ -38,19 +34,18 @@ const VerifyEmail = ({ token }: VerifyEmailProps) => {
           <Image
             src='/hippo-email-sent.png'
             fill
-            alt='the email was sent'
+            alt='Verification email confirmation for zikistorez'
           />
         </div>
 
-        <h3 className='font-semibold text-2xl'>
-          You&apos;re all set!
-        </h3>
+        <h3 className='font-semibold text-2xl'>You&apos;re all set!</h3>
         <p className='text-muted-foreground text-center mt-1'>
           Thank you for verifying your email.
         </p>
         <Link
           className={buttonVariants({ className: 'mt-4' })}
-          href='/sign-in'>
+          href='/sign-in'
+        >
           Sign in
         </Link>
       </div>
@@ -61,15 +56,15 @@ const VerifyEmail = ({ token }: VerifyEmailProps) => {
     return (
       <div className='flex flex-col items-center gap-2'>
         <Loader2 className='animate-spin h-8 w-8 text-zinc-300' />
-        <h3 className='font-semibold text-xl'>
-          Verifying...
-        </h3>
+        <h3 className='font-semibold text-xl'>Verifying...</h3>
         <p className='text-muted-foreground text-sm'>
           This won&apos;t take long.
         </p>
       </div>
     )
   }
+
+  return null
 }
 
 export default VerifyEmail
