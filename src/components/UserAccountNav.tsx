354fd257d97d4f89e1e8e7524a9891ed363570c6
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-05-31 01:05:12 +0100

    Update UserAccountNav.tsx

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -17,20 +17,13 @@ const UserAccountNav = ({ user }: { user: User }) => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger
-        asChild
-        className='overflow-visible'>
-        <Button
-          variant='ghost'
-          size='sm'
-          className='relative'>
+      <DropdownMenuTrigger asChild className='overflow-visible'>
+        <Button variant='ghost' size='sm' className='relative'>
           My account
         </Button>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent
-        className='bg-white w-60'
-        align='end'>
+      <DropdownMenuContent className='bg-white w-60' align='end'>
         <div className='flex items-center justify-start gap-2 p-2'>
           <div className='flex flex-col space-y-0.5 leading-none'>
             <p className='font-medium text-sm text-black'>
@@ -45,9 +38,7 @@ const UserAccountNav = ({ user }: { user: User }) => {
           <Link href='/sell'>Seller Dashboard</Link>
         </DropdownMenuItem>
 
-        <DropdownMenuItem
-          onClick={signOut}
-          className='cursor-pointer'>
+        <DropdownMenuItem onClick={signOut} className='cursor-pointer'>
           Log out
         </DropdownMenuItem>
       </DropdownMenuContent>
