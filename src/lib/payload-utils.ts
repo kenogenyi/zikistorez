1216e65baa8cd4e5061512e5924e55f5ab92c012
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-06-03 16:50:23 +0100

    Update payload-utils.ts

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -7,8 +7,6 @@ export const getServerSideUser = async (
 ) => {
   const token = cookies.get('payload-token')?.value
 
-  if (!token) return { user: null }
-
   const meRes = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
     {
@@ -18,12 +16,9 @@ export const getServerSideUser = async (
     }
   )
 
-  if (!meRes.ok) return { user: null }
-
   const { user } = (await meRes.json()) as {
     user: User | null
   }
 
   return { user }
 }
-
