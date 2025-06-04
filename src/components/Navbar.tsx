3053a3cd60c21febe4afc0b1e066134bd5d05348
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-06-04 22:41:21 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,17 @@
+// components/Navbar.tsx
+'use client'
+
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { Icons } from './Icons'
 import NavItems from './NavItems'
 import { buttonVariants } from './ui/button'
 import Cart from './Cart'
-import { getServerSideUser } from '@/lib/payload-utils'
-import { cookies } from 'next/headers'
 import UserAccountNav from './UserAccountNav'
 import MobileNav from './MobileNav'
+import { User } from '@/payload-types'
 
-const Navbar = async () => {
-  const nextCookies = cookies()
-  const { user } = await getServerSideUser(nextCookies)
-
+const Navbar = ({ user }: { user: User | null }) => {
   return (
     <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
       <header className='relative bg-white'>
@@ -33,49 +32,28 @@ const Navbar = async () => {
 
               <div className='ml-auto flex items-center'>
                 <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
-                  {user ? null : (
-                    <Link
-                      href='/sign-in'
-                      className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
-                      Sign in
-                    </Link>
-                  )}
-
-                  {user ? null : (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
-                  )}
-
                   {user ? (
-                    <UserAccountNav user={user} />
+                    <>
+                      <UserAccountNav user={user} />
+                      <span className='h-6 w-px bg-gray-200' aria-hidden='true' />
+                    </>
                   ) : (
-                    <Link
-                      href='/sign-up'
-                      className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
-                      Create account
-                    </Link>
-                  )}
-
-                  {user ? (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
-                  ) : null}
-
-                  {user ? null : (
-                    <div className='flex lg:ml-6'>
-                      <span
-                        className='h-6 w-px bg-gray-200'
-                        aria-hidden='true'
-                      />
-                    </div>
+                    <>
+                      <Link
+                        href='/sign-in'
+                        className={buttonVariants({ variant: 'ghost' })}
+                      >
+                        Sign in
+                      </Link>
+                      <span className='h-6 w-px bg-gray-200' aria-hidden='true' />
+                      <Link
+                        href='/sign-up'
+                        className={buttonVariants({ variant: 'ghost' })}
+                      >
+                        Create account
+                      </Link>
+                      <span className='h-6 w-px bg-gray-200' aria-hidden='true' />
+                    </>
                   )}
 
                   <div className='ml-4 flow-root lg:ml-6'>
