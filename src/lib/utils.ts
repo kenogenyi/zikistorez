de07bbe2bded9dbb95c0ecaec9255956b5da3582
ken swag <156316619+kenogenyi@users.noreply.github.com>
2025-06-06 00:35:14 +0100

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -59,7 +59,7 @@ export function constructMetadata({
       creator: '@kenswag',
     },
     icons,
-    metadataBase: new URL('zikistorez-production.up.railway.app'),
+    metadataBase: new URL('https://zikistorez-production.up.railway.app'),
     ...(noIndex && {
       robots: {
         index: false,
